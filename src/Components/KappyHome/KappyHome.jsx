f1895f6544c64b1927cf7438b43c2fb111522d9b
EmekaPaul500 <emeka60050@gmail.com>
2025-10-13 20:38:43 +0100

    Are you commited

--- src/Components/KappyHome/KappyHome.jsx
+++ src/Components/KappyHome/KappyHome.jsx
@@ -1,6 +1,7 @@
 import "./KappyHome.css";
-
 import Header from "../../SubComponents/Header/Header";
+import Cards1 from "../../SubComponents/Cards1/Cards1";
+import Card2 from "../../SubComponents/Card2/Card2";
 
 const KappyHome = () => {
   return (
@@ -33,6 +34,86 @@ const KappyHome = () => {
       </section>
       <section className="kappy-home-third-sec">
         <div></div>
+      </section>
+
+      <section className="kappy-home-fourth-sec">
+        <h1>Why Kampos?</h1>
+        <p>
+          Students miss important updates, gist scatters across different
+          platforms, and there’s no single space just for them. That’s the
+          problem Kampos is solving.
+        </p>
+      </section>
+
+      <section className="kappy-home-fifth-sec">
+        <div className="kappy-home-fifth-sec-div-1">
+          <h1>With Kampos you can ?</h1>
+          <p>
+            👥 Connect with thousands of students on your campus From freshers
+            to finals, from your crush to your homies — everybody jams on
+            Kampos.
+          </p>
+        </div>
+        <div className="kappy-home-fifth-sec-div-2">
+          <Cards1
+            card1Heading="🔥 Be at the center of every trending gist and vibe"
+            card1Text="From rants to banters to viral stories, you’ll never miss what’s popping on campus again."
+            card1Img="frame 1"
+          />
+          <Cards1
+            card1Heading="📰 Stay updated on official school news"
+            card1Text="Catch circulars, memos, and announcements the moment they drop — no more “I no hear.” "
+            card1Img="frame 2"
+          />
+          <Cards1
+            card1Heading="🎓 Flex your Kampos identity"
+            card1Text="Rep your school, flex your major & show off your level — Kampos is your ID card with vibes."
+            card1Img="frame 3"
+          />
+        </div>
+      </section>
+
+      <section className="kappy-home-sixth-sec">
+        <div className="kappy-home-sixth-sec-div-1">
+          <h1>Testimonials</h1>
+          <p>
+            We’re not building Kampos for ourselves or out of boredom — we’re
+            building it for students. That’s why we’ve carried them along at
+            every step of the journey, to truly satisfy their cravings. And
+            guess what? They love it. From the hundreds already in our community
+            to the over a thousand students on our waitlist, the excitement is
+            real. We even asked some of them to share their thoughts about
+            Kampos — here’s what they had to say:
+          </p>
+        </div>
+
+        <div className="kappy-home-sixth-sec-div-2">
+          <Card2 />
+          <Card2 />
+          <Card2 />
+          <Card2 />
+          <Card2 />
+        </div>
+      </section>
+
+      <section className="kappy-home-seventh-sec">
+        <figure>
+          <img src="Images/chefs.png" alt="kappy " />
+        </figure>
+
+        <div>
+          <h1>Who are we?</h1>
+          <p>
+            Kampos is built by students, for students.We built Kampos because we
+            knew students truly needed a space that actually feels like campus —
+            with all the energy and vibes. Already trusted by 1,000+ students
+            across Nigeria.
+          </p>
+          <button>
+            <img src="Images/kappy 1.png" alt="kappy " />
+            Meet the chefs
+          </button>
+        </div>
       </section>
     </main>
   );
---
+++ src/SubComponents/Cards1/Cards1.jsx
@@ -0,0 +1,18 @@
+import "./Cards1.css";
+
+const Cards1 = ({ card1Heading, card1Text, card1Img }) => {
+  return (
+    <div className="cards1-div">
+      <div>
+        <h4>{card1Heading}</h4>
+        <p>{card1Text}</p>
+      </div>
+
+      <figure>
+        <img src={`Images/${card1Img}.png`} alt="" />
+      </figure>
+    </div>
+  );
+};
+
+export default Cards1;
